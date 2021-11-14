1f080c39f2748318e5278f04508b12d712839c8d
Maxim Rubchinsky <maxim@rubchinsky.com>
2021-11-14 22:59:40 +0200

    chrome

--- chrome.bash
+++ chrome.bash
@@ -21,6 +21,6 @@ log_info(){
 log_info Installing Chrome
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -o /tmp/google-chrome-stable_current_amd64.deb
-sudo apt install ./tmp/google-chrome-stable_current_amd64.deb
+sudo apt install /tmp/google-chrome-stable_current_amd64.deb
 
 log_info Chrome installed
