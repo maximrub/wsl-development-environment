0719999def02b152889efe91f662ed4e402ff28e
Maxim Rubchinsky <maxim@rubchinsky.com>
2021-11-15 00:10:37 +0200

    mkdir

--- jetbrains.bash
+++ jetbrains.bash
@@ -21,6 +21,7 @@ log_info(){
 log_info Installing Jetbrains Toolbox app
 
 curl -L https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10685.tar.gz -o /tmp/jetbrains-toolbox-1.22.10685.tar.gz
+mkdir -p ~/.local/share/JetBrains/toolbox
 sudo tar -C ~/.local/share/JetBrains/toolbox -xzf /tmp/jetbrains-toolbox-1.22.10685.tar.gz
 
 log_info Jetbrains Toolbox app installed
