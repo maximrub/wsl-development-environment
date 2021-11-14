42179eb6589a91897a82cced5897f36bb9a99263
Maxim Rubchinsky <maxim@rubchinsky.com>
2021-11-15 01:04:53 +0200

    Jetbrains path

--- jetbrains.bash
+++ jetbrains.bash
@@ -23,5 +23,7 @@ log_info Installing Jetbrains Toolbox app
 curl -L https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.22.10685.tar.gz -o /tmp/jetbrains-toolbox-1.22.10685.tar.gz
 mkdir -p ~/.local/share/JetBrains/toolbox
 tar -C ~/.local/share/JetBrains/toolbox -xzf /tmp/jetbrains-toolbox-1.22.10685.tar.gz --strip-components=1
+mkdir -p ~/.local/share/JetBrains/bin
+echo 'export PATH="~/.local/share/JetBrains/bin:$PATH"' >> ~/.bashrc
 
 log_info Jetbrains Toolbox app installed
